50550a8596d6226c06e7956061f98f7551b7f79c
idris <eyiowuawiidris@gmail.com>
2022-12-25 07:07:02 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,16 @@
 
 int main(void)
 {
-	int n1 = 48;
-	int n2;
-	int com = 44;
+	int n;
+	char ch;
 
-	while (n2 <= 57)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(n1);
-		putchar(n2);
-		if (n1 != 56 || n2 != 57)
-		{
-			putchar(com);
-			putchar(32);
-		}
-		n2 += 1;
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
